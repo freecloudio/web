ad11409d89d6ae0a0abd5d0a22c7f4d4c74450b3
riesinger <mail@pascal-riesinger.de>
2018-11-06 09:56:07 +0100

    deploy using freecloud-bot, temporary access token

--- scripts/ci/push_master.sh
+++ scripts/ci/push_master.sh
@@ -2,6 +2,8 @@
 
 echo "Github Token is: $1"
 
+GH_TOKEN="82fc57268a911057ee1d8268dea6f64c98ac1b2f"
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
@@ -9,5 +11,5 @@ git checkout -b master
 git add --force --all build 
 MESSAGE="CI build: $(git log --format=%B -n 1 $TRAVIS_COMMIT)"
 git commit --message "$MESSAGE"
-git remote add origin-deploy "https://$1:x-oauth-basic@github.com/freecloudio/web.git" > /dev/null 2>&1
+git remote add origin-deploy "https://freecloud-bot:$GH_TOKEN@github.com/freecloudio/web.git" > /dev/null 2>&1
 git push --quiet origin-deploy master
