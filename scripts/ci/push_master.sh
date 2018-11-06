ba5a3e4b424cb77a8841d19905fcf2149654b4ad
riesinger <mail@pascal-riesinger.de>
2018-11-06 10:47:51 +0100

    Of course I still love you

--- scripts/ci/push_master.sh
+++ scripts/ci/push_master.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-echo "Github Token is: $1"
-
 GH_TOKEN=$1
 
 git config --global user.email "freecloud.bot@gmail.com"
 git config --global user.name "freecloud-bot"
+git remote add origin-deploy "https://freecloud-bot:$GH_TOKEN@github.com/freecloudio/web.git" > /dev/null 2>&1
 
 git checkout -b master
+git pull origin-deploy master
+
+npm run build
+
 git add --force --all build 
 MESSAGE="CI build: $(git log --format=%B -n 1 $TRAVIS_COMMIT)"
 git commit --message "$MESSAGE"
-git remote add origin-deploy "https://freecloud-bot:$GH_TOKEN@github.com/freecloudio/web.git" > /dev/null 2>&1
 git push --quiet origin-deploy master
