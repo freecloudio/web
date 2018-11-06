a2b7923f99663492a8756c9496250e5c3edcf785
riesinger <mail@pascal-riesinger.de>
2018-11-06 09:19:29 +0100

    Log GH_TOKEN for now

--- scripts/ci/push_master.sh
+++ scripts/ci/push_master.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Github Token is: $1"
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
@@ -7,5 +9,5 @@ git checkout -b master
 git add --force --all build 
 MESSAGE="CI build: $(git log --format=%B -n 1 $TRAVIS_COMMIT)"
 git commit --message "$MESSAGE"
-git remote add origin-deploy "https://$1@github.com/freecloudio/web.git" > /dev/null 2>&1
+git remote add origin-deploy "https://$1:x-oauth-basic@github.com/freecloudio/web.git" > /dev/null 2>&1
 git push --quiet origin-deploy master
