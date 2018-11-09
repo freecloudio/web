3b71ed4239dfecb3f0b2ff8b6bdabb9dcf1c24e2
riesinger <mail@pascal-riesinger.de>
2018-11-09 17:12:22 +0100

    Start work on structure of settings app

--- src/apps/settings/SettingsApp.tsx
+++ src/apps/settings/SettingsApp.tsx
@@ -1,9 +1,35 @@
+import "./SettingsApp.scss";
 import * as React from "react";
+import { SystemApi, SystemStats } from "src/api";
+import { authStore } from "src/store/AuthStore";
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+import SettingsSidebar from "./SettingsSidebar";
 
+@observer
 class SettingsApp extends React.Component {
+	private systemAPI: SystemApi;
+	@observable private systemStats: SystemStats = {};
+
+	constructor(props: any) {
+		super(props);
+
+		const l = window.location;
+		this.systemAPI = new SystemApi(authStore.getAuthorizedAPIConfiguration(), l.protocol + "//" + l.hostname + ":" + l.port + "/api/v1");
+	}
+
+	public async componentDidMount() {
+		this.systemStats = await this.systemAPI.getSystemStats();
+	}
+
 	public render() {
 		return (
-			<div className="settings-app">Settings</div>
+			<div className="settings-app">
+				<SettingsSidebar isAdmin={true}/>
+				<div className="settings">
+					Running on {this.systemStats.goVersion}
+				</div>
+			</div>
 		);
 	}
 }
