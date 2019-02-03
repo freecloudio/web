776646a7c5349b02d2166a499484f38cc14a8af7
riesinger <mail@pascal-riesinger.de>
2019-02-03 11:32:05 -0800

    Fix jest 24 tests failing

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/*
+ * @todo Remove jest workaround from test.js
+ * @body This workaround was added because of a problem with using jest 24 programmatically. See https://github.com/facebook/jest/issues/7704
+ */
+require('jest/node_modules/jest-cli/build/cli');
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
