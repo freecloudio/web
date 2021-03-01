4a43e11aee23b23bd926c363d56b18d1672a4442
riesinger <mail@pascal-riesinger.de>
2021-03-01 22:36:28 +0100

    Deprecate the style utilities
    While they were a nice idea, they are clunky to use and very inflexible.
    They will be replaced by per-component solutions

--- src/style/modifiers.ts
+++ src/style/modifiers.ts
@@ -1,18 +1,25 @@
 /**
  * This interface is meant for components that should be able to be colorized.
  * Note that the default is inherently given. The options are sorted by descending precedence.
+ * @deprecated
  */
 export interface Colorizable {
 	negative?: boolean;
 	primary?: boolean;
 }
 
+/**
+ * @deprecated
+ */
 export enum Size {
 	Default,
 	Small,
 	Large,
 }
 
+/**
+ * @deprecated
+ */
 export interface Sizable {
 	small?: boolean;
 	large?: boolean;
