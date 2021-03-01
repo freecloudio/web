b85ad548b941fd25a3a1fe4cd5a9b92fcef72de7
riesinger <mail@pascal-riesinger.de>
2021-03-01 22:34:50 +0100

    Overhaul styling of the SidebarFolderItem component
    We now include Heroicons (from heroicons.dev) as a replacement for
    Feather icons. There are more Heroicons and they come in solid and
    outlined styles, which provides more flexibility. Also, they just look
    better IMO :^)

--- src/apps/files/SidebarFolderItem.tsx
+++ src/apps/files/SidebarFolderItem.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Folder } from "react-feather";
+import { FolderOutline } from "@graywolfai/react-heroicons";
 import styled from "styled-components";
 
 interface Props {
@@ -9,12 +9,18 @@ interface Props {
 const StyledLi = styled.li`
 	display: flex;
 	align-items: center;
+	margin: 0.75rem 0 0.75rem 0.5rem;
+`;
+
+const StyledLabel = styled.span`
+	display: inline-block;
+	margin-left: 1rem;
 `;
 
 const SidebarFolderItem = ({ children }: Props) => (
 	<StyledLi>
-		<Folder />
-		<span>{children}</span>
+		<FolderOutline width="1.5rem" />
+		<StyledLabel>{children}</StyledLabel>
 	</StyledLi>
 );
 
