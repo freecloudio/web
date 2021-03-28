72b6f2568006eae373db2f9cdfcbe108acc4ba18
riesinger <mail@pascal-riesinger.de>
2021-03-28 18:11:38 +0200

    feat(components): add Card component
    This represents a piece of encapsulated information. It is basically a
    colorable box with an elevation (shadow).
    
    Some features might be missing, but it should be usable already.

---
+++ src/components/Card.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Box from "./Box";
+import { ButtonProps } from "./Button";
+
+interface elevationType {
+	[idx: number]: string;
+}
+
+const elevationMap: elevationType = [
+	"none",
+	"var(--shadow)",
+	"var(--shadow-md)",
+	"var(--shadow-lg)",
+];
+
+const backgroundMap = {
+	default: "var(--color-background)",
+	alt: "var(--color-background-alt)",
+	primary: "var(--color-primary)",
+};
+
+const colorMap = {
+	default: "var(--color-text)",
+	alt: "var(--color-text)",
+	primary: "var(--color-text-on-primary)",
+};
+
+interface ExtraProps {
+	elevation?: keyof typeof elevationMap;
+	color?: keyof typeof backgroundMap;
+}
+
+export type CardProps = ButtonProps | ExtraProps;
+
+const Card = styled(Box)<ExtraProps>`
+	border-radius: var(--rounded);
+	box-shadow: ${({ elevation }) => elevationMap[elevation || 1]};
+	width: auto;
+	height: auto;
+	padding: 1rem;
+	color: var(--color-text);
+	background-color: ${({ color }) => backgroundMap[color || "default"]};
+	color: ${({ color }) => colorMap[color || "default"]};
+`;
+
+export default Card;
