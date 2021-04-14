7ad7113cd3481500434c0fe3c8fe20e826467801
riesinger <mail@pascal-riesinger.de>
2021-04-15 00:23:58 +0200

    feat(components): improve the session handling in ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,18 +3,20 @@ import { Route, Redirect } from "react-router-dom";
 import useUser from "../api/hooks/useUser";
 
 function ProtectedRoute(props: RouteProps) {
-	const { user } = useUser();
-	if (user) {
-		return <Route {...props} />;
+	const { loggedOut } = useUser();
+	if (loggedOut) {
+		console.log("Route at path", props.path, "will redirect to signin");
+		// FIXME: Extract the login page path to a constant
+		return (
+			<Route {...props}>
+				<Redirect
+					to={`/login?goTo=${encodeURIComponent(props.path?.toString() || "")}`}
+				/>
+			</Route>
+		);
 	}
-	// FIXME: Extract the login page path to a constant
-	return (
-		<Route {...props}>
-			<Redirect
-				to={`/login?goTo=${encodeURIComponent(props.path?.toString() || "")}`}
-			/>
-		</Route>
-	);
+	console.log("Route at path", props.path, "rendered, user is authenticated");
+	return <Route {...props} />;
 }
 
 export default ProtectedRoute;
