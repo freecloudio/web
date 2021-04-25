80aff1b3bcc12b3da69efde85fab9765bed8e8c8
riesinger <mail@pascal-riesinger.de>
2021-04-25 19:53:19 +0200

    feat(components): add new AvatarSack component

---
+++ src/components/AvatarStack.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AvatarStack = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+
+  > *:not(:first-child) {
+    margin-right: -0.75rem;
+  }
+`;
+
+export default AvatarStack;
