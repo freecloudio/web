fbd4e90ac287b4a2fefb937084de156c516b6389
riesinger <mail@pascal-riesinger.de>
2021-04-27 21:17:56 +0200

    ui(components): remove the background from IconNav

--- src/components/IconNav.tsx
+++ src/components/IconNav.tsx
@@ -5,44 +5,42 @@ import { default as Box, BoxProps } from "./Box";
 import IconButton from "./IconButton";
 
 export type NavItem = {
-	path: string;
-	name: string;
-	icon: Function;
+  path: string;
+  name: string;
+  icon: Function;
 };
 
 interface ExtraProps extends BoxProps {
-	items: NavItem[];
+  items: NavItem[];
 }
 
 export type IconNavProps = ExtraProps;
 
-const StyledNav = styled(Box)`
-	background: var(--color-primary);
-`;
+const StyledNav = styled(Box)``;
 
 const StyledLink = styled.a`
-	width: 3rem;
-	height: 3rem;
+  width: 3rem;
+  height: 3rem;
 `;
 
 function IconNav({ items, ...props }: ExtraProps) {
-	const location = useLocation();
-	return (
-		<StyledNav as="nav" {...props}>
-			{items.map((item) => (
-				<Link
-					to={item.path}
-					component={StyledLink}
-					title={item.name}
-					key={item.name}
-				>
-					<IconButton toggled={location.pathname.startsWith(item.path)}>
-						<item.icon />
-					</IconButton>
-				</Link>
-			))}
-		</StyledNav>
-	);
+  const location = useLocation();
+  return (
+    <StyledNav as="nav" justify="start" {...props}>
+      {items.map((item) => (
+        <Link
+          to={item.path}
+          component={StyledLink}
+          title={item.name}
+          key={item.name}
+        >
+          <IconButton toggled={location.pathname.startsWith(item.path)}>
+            <item.icon />
+          </IconButton>
+        </Link>
+      ))}
+    </StyledNav>
+  );
 }
 
 export default IconNav;
