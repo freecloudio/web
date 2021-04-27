12b2fca29d48dd94fb3a770124e48a8d5f6b523e
riesinger <mail@pascal-riesinger.de>
2021-04-27 21:13:34 +0200

    ui(components): make text in Cards use the primary color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,43 +3,43 @@ import Box from "./Box";
 import { ButtonProps } from "./Button";
 
 interface elevationType {
-	[idx: number]: string;
+  [idx: number]: string;
 }
 
 const elevationMap: elevationType = [
-	"none",
-	"var(--shadow)",
-	"var(--shadow-md)",
-	"var(--shadow-lg)",
+  "none",
+  "var(--shadow)",
+  "var(--shadow-md)",
+  "var(--shadow-lg)",
 ];
 
 const backgroundMap = {
-	default: "var(--color-background)",
-	alt: "var(--color-background-alt)",
-	primary: "var(--color-primary)",
+  default: "var(--color-background)",
+  alt: "var(--color-background-alt)",
+  primary: "var(--color-primary)",
 };
 
 const colorMap = {
-	default: "var(--color-text-secondary)",
-	alt: "var(--color-text-secondary)",
-	primary: "var(--color-text-on-primary)",
+  default: "var(--color-text-primary)",
+  alt: "var(--color-text-primary)",
+  primary: "var(--color-text-on-primary)",
 };
 
 interface ExtraProps {
-	elevation?: keyof typeof elevationMap;
-	color?: keyof typeof backgroundMap;
+  elevation?: keyof typeof elevationMap;
+  color?: keyof typeof backgroundMap;
 }
 
 export type CardProps = ButtonProps | ExtraProps;
 
 const Card = styled(Box)<ExtraProps>`
-	border-radius: var(--rounded);
-	box-shadow: ${({ elevation }) => elevationMap[elevation || 1]};
-	width: auto;
-	height: auto;
-	padding: 1rem 2rem;
-	background-color: ${({ color }) => backgroundMap[color || "default"]};
-	color: ${({ color }) => colorMap[color || "default"]};
+  border-radius: var(--rounded);
+  box-shadow: ${({ elevation }) => elevationMap[elevation || 1]};
+  width: auto;
+  height: auto;
+  padding: 1rem 2rem;
+  background-color: ${({ color }) => backgroundMap[color || "default"]};
+  color: ${({ color }) => colorMap[color || "default"]};
 `;
 
 export default Card;
