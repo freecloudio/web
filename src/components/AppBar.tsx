47ccf0cfeba74d8fb26c82cf1a607998143c64a7
riesinger <mail@pascal-riesinger.de>
2021-04-27 21:16:37 +0200

    ui(components): make the AppBar horizontal and not have a background

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -9,45 +9,44 @@ import Box from "./Box";
 import IconNav from "./IconNav";
 
 const StyledAppBar = styled(Box)`
-	background: var(--color-primary);
-	color: var(--color-text-on-primary);
-	height: 100vh;
-	width: 5rem;
-	line-height: 4rem;
-	padding: 1rem 0;
+  color: var(--color-text-on-primary);
+  width: 100vw;
+  height: 5rem;
+  padding: 1rem 2rem;
 `;
 
 const LogoContainer = styled(Box)`
-	width: 3rem;
-	height: 3rem;
-	background: var(--color-text-on-primary);
-	border-radius: 2rem;
-	color: var(--color-primary);
+  width: 3rem;
+  height: 3rem;
+  background: var(--color-text-on-primary);
+  border-radius: 2rem;
+  color: var(--color-primary);
+  margin-right: 2rem;
 `;
 
 const appNavItems = Object.entries(apps).map(([app, config]) => ({
-	name: app,
-	path: config.routePrefix,
-	icon: config.icon,
+  name: app,
+  path: config.routePrefix,
+  icon: config.icon,
 }));
 
 export default function AppBar() {
-	const { mutate } = useUser();
-	async function onAvatarClicked() {
-		await logout();
-		mutate();
-	}
-	return (
-		<StyledAppBar direction="col" as="header" justify="between">
-			<LogoContainer>
-				<Logo />
-			</LogoContainer>
-			<IconNav direction="col" items={appNavItems} gap="xl" shrink />
-			<Avatar
-				name="John Doe"
-				imageSrc="https://randomuser.me/api/portraits/men/17.jpg"
-				onClick={onAvatarClicked}
-			/>
-		</StyledAppBar>
-	);
+  const { mutate } = useUser();
+  async function onAvatarClicked() {
+    await logout();
+    mutate(null);
+  }
+  return (
+    <StyledAppBar as="header" justify="between">
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+      <IconNav items={appNavItems} gap="sm" shrink />
+      <Avatar
+        name="John Doe"
+        imageSrc="https://randomuser.me/api/portraits/men/17.jpg"
+        onClick={onAvatarClicked}
+      />
+    </StyledAppBar>
+  );
 }
