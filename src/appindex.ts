1b96c5f707baac983b9dc6e10196e04676a09f9f
riesinger <mail@pascal-riesinger.de>
2021-04-27 21:13:03 +0200

    ui: use the folder icon for the files app

--- src/appindex.ts
+++ src/appindex.ts
@@ -1,27 +1,27 @@
 import {
-	DocumentOutline,
-	CalendarOutline,
-	PhotographOutline,
-	CheckCircleOutline,
+  FolderOutline,
+  CalendarOutline,
+  PhotographOutline,
+  CheckCircleOutline,
 } from "./icons";
 
 const apps = {
-	files: {
-		routePrefix: "/files",
-		icon: DocumentOutline,
-	},
-	calendar: {
-		routePrefix: "/calendar",
-		icon: CalendarOutline,
-	},
-	photos: {
-		routePrefix: "/photos",
-		icon: PhotographOutline,
-	},
-	tasks: {
-		routePrefix: "/tasks",
-		icon: CheckCircleOutline,
-	},
+  files: {
+    routePrefix: "/files",
+    icon: FolderOutline,
+  },
+  calendar: {
+    routePrefix: "/calendar",
+    icon: CalendarOutline,
+  },
+  photos: {
+    routePrefix: "/photos",
+    icon: PhotographOutline,
+  },
+  tasks: {
+    routePrefix: "/tasks",
+    icon: CheckCircleOutline,
+  },
 };
 
 export default apps;
