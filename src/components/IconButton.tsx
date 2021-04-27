e973044a67e203a1ef5fb80265e9e6f657612a05
riesinger <mail@pascal-riesinger.de>
2021-04-27 21:17:25 +0200

    ui(components): change the colors of IconButtons

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,28 +1,29 @@
 import styled from "styled-components";
 
 const IconButton = styled.button<{ toggled?: boolean }>`
-	display: inline-flex;
-	background: ${(props) =>
-		props.toggled ? "var(--color-primary-muted)" : "transparent"};
-	color: white;
-	border: none;
-	border-radius: 100%;
-	width: 3rem;
-	height: 3rem;
-	outline: none;
-	appearance: none;
-	cursor: pointer;
-	box-sizing: border-box;
-	font-family: inherit;
-	font-weight: inherit;
-	text-align: left;
-	align-items: center;
-	justify-content: center;
-	transition: all 0.1s ease-in-out;
+  display: inline-flex;
+  background: ${({ toggled }) =>
+    toggled ? "var(--color-background-alt)" : "transparent"};
+  color: ${({ toggled }) =>
+    toggled ? "var(--color-primary)" : "hsl(var(--palette-gray-00))"};
+  border: none;
+  border-radius: 100%;
+  width: 3rem;
+  height: 3rem;
+  outline: none;
+  appearance: none;
+  cursor: pointer;
+  box-sizing: border-box;
+  font-family: inherit;
+  font-weight: inherit;
+  text-align: left;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.1s ease-in-out;
 
-	&:hover {
-		background: var(--color-primary-muted);
-	}
+  &:hover {
+    background: var(--color-background-alt);
+  }
 `;
 
 export default IconButton;
